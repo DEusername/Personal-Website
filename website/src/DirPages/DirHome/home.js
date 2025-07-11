1c031ecc506d4414074ec0719d38016f7a4cd8e1
Duncan Everson <duncan.everson@icloud.com>
2025-07-10 22:00:20 -0700

    added text and adjusted fonts for website.

--- website/src/DirPages/DirHome/home.js
+++ website/src/DirPages/DirHome/home.js
@@ -1,8 +1,27 @@
 function Home() {
     return (
         <div>
-            <h1>Hello From Home</h1>
+            <div>
+                <h1 className="text-center font-bold text-shadow-sm text-4xl m-4">Home Page</h1>
+            </div>
+
+            {/* This will be a carousel or whatever it is called of pictures of me. */}
+
+            <div>
+                <p>
+                    Hello, and welcome to my website! My name is Duncan Everson, and I've created this
+                    website to act as a storage for my professional projects. I've also included
+                    some facts about my character and skills on this site too, so feel free to
+                    also examine those. If you would like to reach out to me, please feel free to
+                    email me.
+                </p>
+
+                <p>
+                    Enjoy my website!
+                </p>
+            </div>
         </div>
+
     );
 }
 
