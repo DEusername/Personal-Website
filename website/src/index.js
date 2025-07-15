8bce9837ed0fad6c6d3b8f45ebf6a19deaab1467
Duncan Everson <duncan.everson@icloud.com>
2025-07-15 14:51:45 -0700

    rearranged file structure a bit to be more intuitive.

--- website/src/index.js
+++ website/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './DirApp/App.js';
 import reportWebVitals from './reportWebVitals.js';
+
+import './styles/about.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
