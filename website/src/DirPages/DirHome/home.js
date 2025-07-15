4f7c4e4a29323aaf9ee8106bfcf7fc4dace24dbd
Duncan Everson <duncan.everson@icloud.com>
2025-07-15 14:24:17 -0700

    added an image to my home page, and finalized the home page elements.

--- website/src/DirPages/DirHome/home.js
+++ website/src/DirPages/DirHome/home.js
@@ -1,3 +1,5 @@
+import styles from './home.module.css'
+
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -5,22 +7,28 @@ function Home() {
         <div>
             <main>
                 <div>
-                    <h1 className="text-center font-bold text-shadow-sm text-4xl m-4">Home Page</h1>
+                    <h1 className="text-center font-bold text-shadow-sm text-4xl m-4">Home</h1>
                 </div>
 
                 <div>
-                    <p className="bubble">
-                        Hello, and welcome to my website! My name is Duncan Everson, and I've created this
-                        website to act as a storage for my professional projects. I've also included
-                        some facts about my character and skills on this site too, so feel free to
-                        also examine those. If you would like to reach out to me, please feel free to
-                        email me at duncan.everson@icloud.com.
-                    </p>
+                    <div className='flex flex-row gap-4 justify-center'>
+                        <p className="bubble text-left p-6 w-2/5">
+                            Hello, and welcome to my website! My name is Duncan Everson, and I've created this
+                            website to act as a storage for my professional projects. I've also included
+                            some facts about my character and skills on this site too, so feel free to
+                            also examine those. If you would like to reach out to me, please
+                            email me at <span className='font-bold'>duncan.everson@icloud.com</span>,
+                            or by clicking the email button.
+                        </p>
 
-                    {/* This will be just a professional looking picture of me. */}
+                        <img src="images/professional.jpeg" alt="A man with glass and 
+                        brown hair in a suit at a high class venue"
+                            className='w-72 rounded-2xl'>
+                        </img>
+                    </div>
 
                     <div className="dropdown dropdown-bottom relative">
-                        <button className="mt-4 text-xl  btn bg-neonblue hover:bg-blue-100">
+                        <button className="mt-4 text-xl shadow-lg btn bg-neonblue hover:bg-blue-100">
                             Please enjoy reading over my website!
                         </button>
 
@@ -32,7 +40,6 @@ function Home() {
                             <li><Link to="/business-projects">Business Projects</Link></li>
                         </ul>
                     </div>
-
                 </div>
             </main>
         </div>
