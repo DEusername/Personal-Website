e2b8e6f7d90791ea6183e8085bc3b68dfe0079a5
Duncan Everson <duncan.everson@icloud.com>
2025-07-24 21:34:18 -0700

    made the card more dynamic and allow for pdf items to be viewable by clicking a button.

--- website/src/components/card.js
+++ website/src/components/card.js
@@ -1,21 +1,32 @@
-function card() {
+import { useState } from 'react';
+
+function Card({ title, content, linkDivs }) {
+
+    const [showPDF, setShowPDF] = useState(false);
+
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card bg-white w-96 shadow-sm">
             <figure className="px-10 pt-10">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"
+                    src=""
+                    alt=""
                     className="rounded-xl" />
             </figure>
             <div className="card-body items-center text-center">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <h3 className="card-title">{title}</h3>
+                <p>{content}</p>
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button
+                        className="btn btn-primary font-bold"
+                        onClick={() => setShowPDF(!showPDF)}
+                    >
+                        {showPDF ? 'Hide PDF' : 'View PDF'}
+                    </button>
                 </div>
+                {showPDF ? linkDivs : null}
             </div>
         </div>
     );
 }
 
-export default card;
+export default Card;
