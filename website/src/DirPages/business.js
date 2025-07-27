b3d0bde2601f4378802e0fca5e1508f68ca197b6
Duncan Everson <duncan.everson@icloud.com>
2025-07-27 00:12:47 -0700

    stubbed out business and misc pages.

--- website/src/DirPages/business.js
+++ website/src/DirPages/business.js
@@ -1,7 +1,101 @@
+import Card from '../components/card.js'
+import PDFViewer from '../components/pdf_viewer.js';
+
 function Business() {
     return (
         <div>
-            <h1>Hello From Business</h1>
+            <h1 className="text-center font-bold text-shadow-sm text-4xl m-4">Business Projects</h1>
+
+            <main>
+                <div className="flex gap-4 mb-4">
+                    <Card
+                        title="Solar Roadways Innovation Management Project"
+                        content=""
+                        linkDivs={
+                            <div>
+                                <PDFViewer
+                                    pdfUrl={"./projects/BA_Projects/Team-6_Solar-Roadways_BA363.pdf"}
+                                    label={"Report"}
+                                />
+                            </div>}
+                    />
+                    <Card
+                        title="IB IA (Business Management SL) - Courthouse Club
+                        Fitness"
+                        content="International Baccalaureate Internal Assessment submitted for Standard
+                        Level consideration in Business Management. I scored a 7/7."
+                        linkDivs={
+                            <div>
+                                <PDFViewer
+                                    pdfUrl={"./projects/IAs/Business_Management_IA.pdf"}
+                                    label={"Report"}
+                                />
+                            </div>}
+                    />
+                    <Card
+                        title="Integrated Marketing Campaign (Event) - E3"
+                        content=""
+                        linkDivs={
+                            <div>
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/E3/E3_Written.pdf"}
+                                    label={"Written Project"}
+                                />
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/E3/E3_Slideshow.pdf"}
+                                    label={"Presentation"}
+                                />
+                            </div>}
+                    />
+                </div>
+                <div className="flex gap-4 mb-4 justify-center">
+                    <Card
+                        title="Integrated Marketing Campaign (Service) - Marriott International"
+                        content=""
+                        linkDivs={
+                            <div>
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/Marriott_International/Marriott_Written.pdf"}
+                                    label={"Written Project"}
+                                />
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/Marriott_International/Marriott_Slideshow.pdf"}
+                                    label={"Presentation"}
+                                />
+                            </div>}
+                    />
+                    <Card
+                        title="International Business Plan - Boschi Viaggio"
+                        content=""
+                        linkDivs={
+                            <div>
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/Boschi_Viaggio/Boschi_Viaggio_Written.pdf"}
+                                    label={"Written Project"}
+                                />
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/Boschi_Viaggio/Boschi_Viaggio_Slideshow.pdf"}
+                                    label={"Slideshow"}
+                                />
+                            </div>}
+                    />
+                    <Card
+                        title="Integrated Marketing Campaign (Service) - AirBnB"
+                        content=""
+                        linkDivs={
+                            <div>
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/AirBnB/AirBnB_Written.pdf"}
+                                    label={"Written Project"}
+                                />
+                                <PDFViewer
+                                    pdfUrl={"./projects/DECA_Projects/AirBnB/AirBnB_Slideshow.pdf"}
+                                    label={"Presentation"}
+                                />
+                            </div>}
+                    />
+                </div>
+            </main>
         </div>
     );
 }
--- website/src/DirPages/misc.js
+++ website/src/DirPages/misc.js
@@ -1,7 +1,43 @@
+import Card from '../components/card.js'
+
 function Misc() {
     return (
         <div>
-            <h1>Hello From Miscellaneous</h1>
+            <h1 className="text-center font-bold text-shadow-sm text-4xl m-4">Miscellaneous Projects</h1>
+
+            <main>
+                <div className="flex gap-4 mb-4">
+                    <Card
+                        title="IB IA (Math SL) - Monopoly Probability Study"
+                        content="International Baccalaureate Internal Assessment submitted for Standard
+                        Level consideration in Math. I scored a 7/7."
+                        linkDivs={
+                            <div>
+                                <iframe src="./projects/IAs/Math_IA.pdf"
+                                    width="100%" height="400px"></iframe>
+                            </div>}
+                    />
+                    <Card
+                        title="IB IA (Physics HL) - Coils and Electromagnetic Field Strength"
+                        content="International Baccalaureate Internal Assessment submitted for Higher
+                        Level consideration in Physics. I scored a 4/7."
+                        linkDivs={
+                            <div>
+                                <iframe src="./projects/IAs/Physics_IA.pdf"
+                                    width="100%" height="400px"></iframe>
+                            </div>}
+                    />
+                    <Card
+                        title="Technical Paper - Optimal Teaching Method"
+                        content=""
+                        linkDivs={
+                            <div>
+                                <iframe src=""
+                                    width="100%" height="400px"></iframe>
+                            </div>}
+                    />
+                </div>
+            </main>
         </div>
     );
 }
